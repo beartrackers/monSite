e89d7ccd02b81b5420a22f493b2d4377a5e6cae6
beartrackers <nolf199@gmail.com>
2016-03-31 19:21:10 +0200

    Added files via upload

---
+++ JS/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $('#fullpage').fullpage({
+        anchors:['home','about', 'work','contact'],
+        keyboardScrolling: false,
+        scrollOverflow: true,
+        onLeave : function(){
+            $(".main-menu").addClass("hide-menu");
+        }
+    });
+
+    $('.slider').slick({  dots: true, arrows : false });
+
+    $(".btn-toggle-menu").click(function() {
+        $(".main-menu").toggleClass("hide-menu");
+    })
+
+});
